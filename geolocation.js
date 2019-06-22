de3627b6cf24cbd39e3177878e4572d1d6d23049
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-06-21 22:29:01 -0400

    Working on getting geolocation lat/long coords.

--- geolocation.js
+++ geolocation.js
@@ -1,37 +1,36 @@
-
-//because of the way javascript loads, the global variable isnt storing data... you have to have it run when the page is fully loaded. Use JQuwry $(document.ready() or find alternative for async information... maybe using a Promise?
-//Geolocatoin is async so it will not return the results immediately, using a promise will fix that. 
-   
-
-    //Promise using async to retrieve data location
-    //Object would return empty data set before geolocation data populated
-    const getGeoPosition = new Promise((resolve, reject) => {
-
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(resolve, reject);
-        }else{
-            reject({
-                error: 'browser doesn\'t support geolocation'
-            })
-        }
-    });
-
-    getGeoPosition.then((data)=>{
-        let geoPosition = {};
-
-        //assign needed data points to geoPosition object
-        geoPosition['latitude'] = data.coords.latitude;
-        geoPosition['longitude'] = data.coords.longitude;
-        geoPosition['timeStamp'] = new Date(data.timestamp).toLocaleString();
+let getGeoPosition = () => {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(geoSuccess);
         
-        console.log(geoPosition);
-
-        return geoPosition;
-
-    }).catch((error) => {
-        console.log('Error', error);
-    });
+    }else{
+        document.getElementById('message').innerHTML = 'browser doesn\'t support geolocation';
+    }
+}
 
 
+let geoSuccess = (position) => {
+    console.log(position);
+    let geoDataItem = document.querySelectorAll('.geoData');
+    let latPos = position.coords.latitude;
+    let longPos = position.coords.longitude;
+    let timeStamp = new Date(position.timestamp).toLocaleString();
 
+    geoDataItem[0].innerHTML = 'Latitude: ' + latPos;
+    geoDataItem[1].innerHTML = 'Longitude: ' + longPos;
+    geoDataItem[2].innerHTML = 'Time: ' + timeStamp;
 
+    coordsData(latPos, longPos, timeStamp);
+    
+}
+
+let coordsData = (lat, long, time) => {
+    return {
+        latitude: lat,
+        longitude: long,
+        time: time
+    }
+}
+
+console.log(coordsData());
+
+document.getElementById('geoBtn').addEventListener('click', getGeoPosition);
