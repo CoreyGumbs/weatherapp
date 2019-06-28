27822a11fa1c8a724cd9d30511fee0db6966ce3a
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-06-28 17:27:13 -0400

    Added helper functions for weather data and push them DOM elements.

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,5 +1,14 @@
+window.addEventListener('load', () =>{
+    geoFindMe();
+});
 //current coordinates variable placeholder.
 coordsArray = [];
+
+let errorMsg = (error) => {
+    console.log('Error Code: ' + error.code);
+    console.log('Error Msg: ' + error.message);
+    console.error(error);
+}
 
 //Finds the geolocation of user.
 let geoFindMe = () => {
@@ -9,24 +18,16 @@ let geoFindMe = () => {
 
         //getWeatherData() takes the api proxy url and adds, the latitude, longitude coordinates from retrieved geolocation position.
         //these coordinates are needed to access the Dark Sky endpoint weather api. 
-        getWeatherData(`https://coreygumbs-eval-test.apigee.net/localweather/${position.coords.latitude},${position.coords.longitude}`)
+        fetchWeatherData(`https://coreygumbs-eval-test.apigee.net/localweather/${position.coords.latitude},${position.coords.longitude}`)
         .catch(error =>{
-            console.log('Error Code: ' + error.code);
-            console.log('Error Msg: ' + error.message);
-            console.error(error);
+            errorMsg(error);
         });
-    }
-
-    //error callback for navigator.geolocation.getCurrentPosition() parameter.
-    let geoError = error =>{
-        console.log('Error Code: ' + error.code);
-        console.log('Error Msg: ' + error.message);
     }
 
     //checks if browser supports geolocation.
     //passes both the success & error callbacks from above into the navigator.geolocation.getCurrentPosition() parameters.
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+        navigator.geolocation.getCurrentPosition(geoSuccess, errorMsg);
     }else{
         document.getElementById('errMsg').innerHTML = 'browser doesn\'t support geolocation';
     }
@@ -35,24 +36,30 @@ let geoFindMe = () => {
 
 //takes url from the success callback in geoFindMe()
 //uses async/await for data retrieval from Dark Sky API.
-let getWeatherData = async url => {
+let fetchWeatherData = async url => {
     //fetches url from Dark Sky API endpoint
-    const response = await fetch(url);
+    const response = await fetch(url)
+    .catch(error =>{
+        errorMsg(error);
+    });
 
     //converts response body to json.
-    const weatherData = await response.json();
+    const weatherData = await response.json()
+    .catch(error =>{
+        errorMsg(error);
+    });
 
     //getGeoCords takes the latitude & longitude coordinates from API as paramaters.
     getGeoCoords(weatherData.latitude, weatherData.longitude)
     .catch(error =>{
-        console.log('Error Code: ' + error.code);
-        console.log('Error Msg: ' + error.message);
-        console.error(error);
+        errorMsg(error);
     });
 
     console.log(weatherData);
+    getCurrentTemperatures(weatherData.currently.temperature, weatherData.currently.apparentTemperature);
+    getCurrentTimeStamp(weatherData.currently.time);
 
-    document.getElementById("weatherData").innerHTML = weatherData.currently.summary + '<br />' + `Temperature: ${Math.round(weatherData.currently.temperature)} &deg;`;
+
 }
 
 //Function to push recieved latitude & longitude coordinates from API call to placeholder array.
@@ -63,3 +70,34 @@ let getGeoCoords = async (lat, long) => {
 
 //Event Listener for when user clicks check weather button.
 document.getElementById('geoBtn').addEventListener('click', geoFindMe);
+
+
+let getCurrentTemperatures = (temperature, feelsLikeTemp) => {
+    const currentTemp =  Math.round(temperature);
+    const feelsTemp =  Math.round(feelsLikeTemp);
+    document.getElementById("current-temp").innerHTML = `<h1> ${currentTemp}&deg;</h1>`;
+    document.getElementById("feels-like-temp").innerHTML = `<p>Feels Like: <span> ${feelsTemp}&deg;</span></p<`;
+}
+
+let getCurrentTimeStamp = (time) => {
+    const unixtimestamp = time;
+    const currentTime = new Date(unixtimestamp*1000);
+    document.getElementById("retrieved-time").innerHTML= `retrieved: ${currentTime}`;
+}
+
+let getCurrentHumidity = '';
+/* TODO List
+
+--Create a lib for each weather category that returns the html. (IE: current weather, current weather image, hourly forecast etc.)
+Make them components?
+
+--Try to make it as dynamic as possible. 
+
+--Design layout
+
+--add current location to map
+
+--create database to save all generated data in
+
+
+*/
